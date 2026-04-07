2aea4418be4b66a4ec5aa77d7ce716e0020c8d87
dipak-badu <dipakbadu2061@gmail.com>
2026-04-07 10:49:56 +0545

    created user expense and income schema

---
+++ server/src/models/user.model.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    income: {
+      type: Number,
+      default: 0,
+    },
+    monthlyIncome: {
+      type: Number,
+      default: 0,
+    },
+    avgMonthlyIncome: {
+      type: Number,
+      default: 0,
+    },
+    expenses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Expense",
+      },
+    ],
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("User", userSchema);

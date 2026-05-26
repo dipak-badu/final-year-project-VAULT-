926a35c98f59bd4f61de70d96b67155b0d2de6d9
dipak-badu <dipakbadu2061@gmail.com>
2026-05-26 10:01:50 +0545

    feat: add RegisterPage component and update routing to include login path

--- client/src/config/Router.jsx
+++ client/src/config/Router.jsx
@@ -1,15 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
+import RegisterPage from "../pages/RegisterPage";
 
 const routes = [
   { path: "/", element: <LandingPage /> },
+
+  { path: "/login", element: <RegisterPage /> },
+
   {
     path: "*",
-    element: (
-      <>
-        <h1>404 Not Found</h1>
-      </>
-    ),
+    element: <h1>404 Not Found</h1>,
   },
 ];
 
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,7 @@
+export default function RegisterPage() {
+  return (
+    <section className="register-page w-full h-auto flex  bg-black ">
+      <div className=""></div>
+    </section>
+  );
+}

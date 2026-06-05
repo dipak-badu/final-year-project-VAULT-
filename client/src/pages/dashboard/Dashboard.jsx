0dfbeaded13b8d571303587fbfa88a41e51f0dc9
dipak-badu <dipakbadu2061@gmail.com>
2026-06-05 16:41:14 +0545

    style: format import statements and improve code readability in Dashboard component

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,27 +1,27 @@
 import {
-    LayoutDashboard,
-    ReceiptText,
-    Wallet,
-    BarChart3,
-    Settings,
-    CircleHelp,
-    Plus,
-    Search,
+  LayoutDashboard,
+  ReceiptText,
+  Wallet,
+  BarChart3,
+  Settings,
+  CircleHelp,
+  Plus,
+  Search,
 } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import Sidebar from "../../component/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
 
 export default function Dashboard() {
-    return (
-        <div className="flex min-h-screen bg-black text-white">
-            {/* Sidebar */}
-            <Sidebar />
+  return (
+    <div className="flex min-h-screen bg-black text-white md:block">
+      {/* Sidebar */}
+      <Sidebar />
 
-            {/* Main Content */}
-            <main className="flex-1 p-8">
-                <Outlet />
-            </main>
-        </div>
-    );
+      {/* Main Content */}
+      <main className="flex-1 p-8">
+        <Outlet />
+      </main>
+    </div>
+  );
 }

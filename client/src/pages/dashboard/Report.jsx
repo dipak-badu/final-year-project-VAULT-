7f18e16e0666816edf1a2622fb554eb2978e9b20
Sameer-Dangaura <sameerdangaura1@gmail.com>
2026-07-03 17:44:20 +0545

    feat: add Doughnut chart for income and expense sources in Report page

--- client/src/pages/dashboard/Report.jsx
+++ client/src/pages/dashboard/Report.jsx
@@ -18,9 +18,50 @@ defaults.plugins.title.color = "black";
 export default function Report() {
     return (
         <main className="flex flex-col min-h-screen p-4 sm:p-6 md:p-8 gap-10">
-            <div className="grid lg:grid-cols-3 w-full gap-10 max-w-350">
+            <div className="grid lg:grid-cols-2 w-full gap-10 max-w-350">
                 <GridItems title="Monthly Income and Expenses" className="lg:col-span-2">
                     <AreaChart />
+                </GridItems>
+
+                <GridItems title="Income Sources">
+                    <div className="w-75 h-75 mx-auto">
+                        <Doughnut
+                            data={{
+                                labels: sourceData.map((item) => item.label),
+                                datasets: [
+                                    {
+                                        data: sourceData.map((item) => item.value),
+                                        backgroundColor: [
+                                            'rgba(255, 99, 132, 0.2)',
+                                            'rgba(54, 162, 235, 0.2)',
+                                            'rgba(255, 205, 86, 0.2)',
+                                            'rgba(75, 192, 192, 0.2)',
+                                            'rgba(153, 102, 255, 0.2)',
+                                            'rgba(255, 159, 64, 0.2)',
+                                            'rgba(199, 199, 199, 0.2)'
+                                        ],
+                                        borderColor: [
+                                            'rgba(255, 99, 132, 1)',
+                                            'rgba(54, 162, 235, 1)',
+                                            'rgba(255, 205, 86, 1)',
+                                            'rgba(75, 192, 192, 1)',
+                                            'rgba(153, 102, 255, 1)',
+                                            'rgba(255, 159, 64, 1)',
+                                            'rgba(199, 199, 199, 1)'
+                                        ],
+                                        borderWidth: 1
+                                    }
+                                ]
+                            }}
+                            options={{
+                                plugins: {
+                                    legend: {
+                                        position: "bottom",
+                                    },
+                                },
+                            }}
+                        />
+                    </div>
                 </GridItems>
 
                 <GridItems title="Expense Sources">
